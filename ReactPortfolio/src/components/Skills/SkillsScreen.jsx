5c7d1e666fe85d9929a6cfede27f3200584de10d
P Gonz <GonzalesP@csu.fullerton.edu>
2024-05-13 01:01:53 -0700

    Added a background image to replace the plain background color

--- ReactPortfolio/src/components/Skills/SkillsScreen.jsx
+++ ReactPortfolio/src/components/Skills/SkillsScreen.jsx
@@ -16,23 +16,23 @@ export default function SkillsScreen() {
         {/* Divide the Skill components into three horizontal rows */}
         <div className={style.rowContainer}>
           <Skill name="ReactJS" image="/react-logo.png" />
-          <Skill name="HTML5" image="html5-logo.png" />
-          <Skill name="CSS3" image="css3-logo.png" />
-          <Skill name="JavaScript" image="js-logo.png" />
+          <Skill name="HTML5" image="/html5-logo.png" />
+          <Skill name="CSS3" image="/css3-logo.png" />
+          <Skill name="JavaScript" image="/js-logo.png" />
         </div>
 
         <div className={style.rowContainer}>
-          <Skill name="PHP" image="php-logo.png" />
-          <Skill name="MySQL" image="mysql-logo.png" />
-          <Skill name="MongoDB" image="mongodb-logo.png" />
-          <Skill name="Python" image="python-logo.png" />
+          <Skill name="PHP" image="/php-logo.png" />
+          <Skill name="MySQL" image="/mysql-logo.png" />
+          <Skill name="MongoDB" image="/mongodb-logo.png" />
+          <Skill name="Python" image="/python-logo.png" />
         </div>
 
         <div className={style.rowContainer}>
-          <Skill name="C" image="c-lang-logo.png" />
-          <Skill name="C++" image="cpp-logo.png" />
-          <Skill name="C#" image="c-sharp-logo.png" />
-          <Skill name="GitHub" image="github-logo.png" />
+          <Skill name="C" image="/c-lang-logo.png" />
+          <Skill name="C++" image="/cpp-logo.png" />
+          <Skill name="C#" image="/c-sharp-logo.png" />
+          <Skill name="GitHub" image="/github-logo.png" />
         </div>
       </div>
     </Screen>
